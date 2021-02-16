855fc2ef414c250bfd51654711e036b801a78311
Pulpo <pol.gomar@enti.cat>
2021-02-16 15:44:01 +0100

    Connection Established

--- Smols/Assets/Scripts/PlayerSetup.cs
+++ Smols/Assets/Scripts/PlayerSetup.cs
@@ -5,12 +5,24 @@ public class PlayerSetup : NetworkBehaviour {
     [SerializeField]
     Behaviour[] componentsToDisable;
 
+    private Camera sceneCamera;
+
     private void Start() {
         if (!isLocalPlayer) {
             for (int i = 0; i < componentsToDisable.Length; i++) {
                 componentsToDisable[i].enabled = false;
             }
+        } else {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) {
+                sceneCamera.gameObject.SetActive(false);
+            }
         }
     }
 
+    private void OnDisable() {
+        if (sceneCamera != null) {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
 }
